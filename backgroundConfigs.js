558731fa9ab950b07f9afc94de618ecc180d6a06
Ian Chen <ianchen06@gmail.com>
2018-05-19 17:53:54 +0800

    Fix typo

--- backgroundConfigs.js
+++ backgroundConfigs.js
@@ -5,7 +5,7 @@ const SUPPORTED_HOSTS = [
 
 const PARSER_DICT = {
   'www.food123.com.tw': 'food123.js',
-  'www.happy-shopping.tw/': 'happyshopping.js',
+  'www.happy-shopping.tw': 'happyshopping.js',
 }
 
 const REMOTE_SERVER = 'https://mitjobs.ianchenhq.com';
