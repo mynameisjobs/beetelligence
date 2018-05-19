6ccb58bf322f8144d011bee895a2af99898bd8d4
Ian Chen <ianchen06@gmail.com>
2018-05-19 22:02:02 +0800

    Added momoshop

--- backgroundConfigs.js
+++ backgroundConfigs.js
@@ -1,12 +1,14 @@
 const SUPPORTED_HOSTS = [
   'www.food123.com.tw',
   'www.happy-shopping.tw',
+  'www.momoshop.com.tw',
   'shopee.tw'
 ];
 
 const PARSER_DICT = {
   'www.food123.com.tw': 'food123.js',
   'www.happy-shopping.tw': 'happyshopping.js',
+  'www.momoshop.com.tw': 'momoshop.js',
   'shopee.tw':'shopee.js'
 }
 
