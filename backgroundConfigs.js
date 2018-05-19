d1b1c234bef2f1eaf8f0a5db144a045db6c4c6a7
Ian Chen <ianchen06@gmail.com>
2018-05-19 17:51:25 +0800

    Added bear mom haapshopping.tw

--- backgroundConfigs.js
+++ backgroundConfigs.js
@@ -1,9 +1,11 @@
 const SUPPORTED_HOSTS = [
   'www.food123.com.tw',
+  'www.happy-shopping.tw',
 ];
 
 const PARSER_DICT = {
   'www.food123.com.tw': 'food123.js',
+  'www.happy-shopping.tw/': 'happyshopping.js',
 }
 
 const REMOTE_SERVER = 'https://mitjobs.ianchenhq.com';
