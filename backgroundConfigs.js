7ec3eafdbbaa5679f732458406a5380d1f3f76a8
hsuyuming <abego452@gmail.com>
2018-05-19 20:00:30 +0800

    add shopee parser

--- backgroundConfigs.js
+++ backgroundConfigs.js
@@ -1,11 +1,13 @@
 const SUPPORTED_HOSTS = [
   'www.food123.com.tw',
   'www.happy-shopping.tw',
+  'www.shopee.tw'
 ];
 
 const PARSER_DICT = {
   'www.food123.com.tw': 'food123.js',
   'www.happy-shopping.tw': 'happyshopping.js',
+  'www.shopee.tw':'shopee.js'
 }
 
 const REMOTE_SERVER = 'https://mitjobs.ianchenhq.com';
---
+++ static/shopee.js
@@ -0,0 +1,44 @@
+(function() {
+  //const extension_id = 'jagbamlhmfmhepenohcecakjnglojfal';
+  const extension_id = window.beecheaperID;
+
+  function parseData() {
+    const now = new Date();
+    const docs = []
+    document.querySelectorAll('.item-wrap.border-grey.bg-white').forEach(x => {
+      try {
+        const updated_at = now.toISOString();
+        const title = x.querySelector('h2').innerText;
+        const imageurl = x.querySelector('img').src;
+        const url = x.href.split('#')[0];
+        const price = x.querySelector('.food123.price').innerText;
+
+        docs.push({ url, title, imageurl, price,  updated_at, source: 'food123' });
+      }catch(e) {
+        console.log(e);
+      }
+    })
+    return docs
+  }
+
+  function parseDetail() {
+    const now = new Date();
+    const source = 'shopee';
+    const price = document.querySelector('.shopee-product-info__header__real-price').innerText.split("-").length == 1 ? document.querySelector('.shopee-product-info__header__real-price').innerText.split("-")[0]:document.querySelector('.shopee-product-info__header__real-price').innerText.split("-")[1];
+    const title = document.querySelector('h1.shopee-product-info__header__text').innerText;
+    const imageurl = document.querySelector('div._3hw5IP').style.backgroundImage.backgroundImage.match(/url\(["']?([^"']*)["']?\)/)[1];
+    const updated_at = now.toISOString();
+    const url = document.location.href.split('#')[0];
+
+    return { source, title, price, url, imageurl, updated_at };
+  }
+
+  const pageUrl = document.location.href;
+  const data = parseDetail();
+
+  chrome.runtime.sendMessage(extension_id, { action: "postData", data: data }, (response) => {
+    view.set(response.data);
+    view.render();
+  })
+ 
+})();
